11c065969c471d6f6b773feb1e00ba013b400c51
appsea <40578679+appsea@users.noreply.github.com>
2018-09-26 18:21:46 +0100

    Update download.sh

--- .circleci/download.sh
+++ .circleci/download.sh
@@ -1,4 +1,11 @@
-if [[ ${KEYSTORE_DOWNLOAD_URL} ]] then
-        curl -L -o KEYSTORE_FILE_NAME ${KEYSTORE_DOWNLOAD_URL} else
-        echo "No keystore Environment variable found"
+# use curl to download a keystore from $KEYSTORE_URI, if set,
+# to the path/filename set in $KEYSTORE.
+if [[ $KEYSTORE && ${KEYSTORE} && $KEYSTORE_URI && ${KEYSTORE_URI} ]]
+then
+    echo "Keystore detected - downloading..."
+    # we're using curl instead of wget because it will not
+    # expose the sensitive uri in the build logs:
+    curl -L -o ${KEYSTORE} ${KEYSTORE_URI}
+else
+    echo "Keystore uri not set.  .APK artifact will not be signed."
 fi
